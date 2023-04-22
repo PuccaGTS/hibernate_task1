09afcdf0ad9eb3d8f3a5d1a1008ae8ca9a49ddc8
Vladislav Pavlenko <pavlenkoovd@gmail.com>
2023-04-23 01:19:53 +0900

    add query annotation

--- src/main/java/ru/netology/netolhiber1/repository/PersonRepository.java
+++ src/main/java/ru/netology/netolhiber1/repository/PersonRepository.java
@@ -1,6 +1,8 @@
 package ru.netology.netolhiber1.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.netology.netolhiber1.entity.Person;
 import ru.netology.netolhiber1.entity.PersonPK;
 
@@ -8,9 +10,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PersonRepository extends CrudRepository<Person, PersonPK> {
-        public List<Person> getPersonByCity(String city);
+        @Query("select person from Person person where person.city = :city")
+        public List<Person> getPersonByCity(@Param("city") String city);
 
-        public List<Person> getPersonByAgeBeforeOrderByAge(int age);
+        @Query("select person from Person person where person.age<:age order by person.age")
+        public List<Person> getPersonByAgeBeforeOrderByAge(@Param("age") int age);
 
-        public Optional<Person> getPersonByNameAndSurname (String name, String surname);
+        @Query("select person from Person person where person.name = :name AND person.surname = :surname")
+        public Optional<Person> getPersonByNameAndSurname (@Param("name") String name, @Param("surname") String surname);
 }
