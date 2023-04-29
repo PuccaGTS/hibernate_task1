e5e095d7f751e13cb5b78126a9388de71e4c8af2
Vladislav Pavlenko <pavlenkoovd@gmail.com>
2023-04-30 00:02:08 +0900

    add mthod security

--- src/main/java/ru/netology/netolhiber1/config/SecurityConfig.java
+++ src/main/java/ru/netology/netolhiber1/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.netology.netolhiber1.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder encoder(){
@@ -20,22 +22,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("Oleg").password(encoder().encode("pass")).authorities("admin")
+                .withUser("Oleg").password(encoder().encode("pass")).roles("READ")
                 .and()
-                .withUser("Vlad").password(encoder().encode("pass1")).authorities("user");
+                .withUser("Vlad").password(encoder().encode("pass1")).roles("WRITE")
+                .and()
+                .withUser("Semen").password(encoder().encode("pass2")).roles("DELETE");
 
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-city").permitAll()
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-age").hasAuthority("user")
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-name-surname").hasAuthority("admin")
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.formLogin()
+//                .and()
+//                .authorizeRequests().antMatchers("/persons/by-city").permitAll()
+//                .and()
+//                .authorizeRequests().antMatchers("/persons/by-age").hasAuthority("user")
+//                .and()
+//                .authorizeRequests().antMatchers("/persons/by-name-surname").hasAuthority("admin")
+//                .and()
+//                .authorizeRequests().anyRequest().authenticated();
+//    }
 }
--- src/main/java/ru/netology/netolhiber1/controller/NewController.java
+++ src/main/java/ru/netology/netolhiber1/controller/NewController.java
@@ -1,6 +1,9 @@
 package ru.netology.netolhiber1.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,28 +11,42 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.netology.netolhiber1.entity.Person;
 import ru.netology.netolhiber1.repository.PersonRepository;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
-public class PersonController {
+public class NewController {
     private final PersonRepository personRepository;
-
     @GetMapping("persons/by-city")
+    @Secured("ROLE_READ")
     public List<Person> getPersonByCity(@RequestParam String city){
         return personRepository.getPersonByCity(city);
     }
 
     @GetMapping("persons/by-age")
+    @RolesAllowed("ROLE_WRITE")
     public List<Person> getPersonByAge(@RequestParam int age){
         return personRepository.getPersonByAgeBeforeOrderByAge(age);
     }
 
     @GetMapping("persons/by-name-surname")
+    @PreAuthorize("hasAnyRole('ROLE_WRITE') or hasAnyRole('ROLE_DELETE')")
     public Optional<Person> getPersonByNameSurname(@RequestParam String name, @RequestParam String surname){
         return personRepository.getPersonByNameAndSurname(name, surname);
     }
+
+    @GetMapping("get")
+    public String helloUsername(@RequestParam String username){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getName().equals(username)){
+            return "Hello " + username;
+        } else {
+            return null;
+        }
+    }
+
 
 }
